f13427a0337158322037e95ae53d07ebfdd00d3a
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-04-25 22:24:14 -0300

    Fixing Connection Failure Error

--- App/mySecurity/request.py
+++ App/mySecurity/request.py
@@ -2,13 +2,16 @@ import requests
 import globals
 
 def sendRequest():
-    response = requests.post(
-        globals.REMOTE_HOST_ADDRESS,
-        json={
-            'instance_name': globals.LOCAL_HOST,
-            'instance_ip':  globals.LOCAL_HOST_ADDRESS,
-            'instance_secret': globals.SECRET
-        }
-    )
+    try:
+        response = requests.post(
+            globals.REMOTE_HOST_ADDRESS,
+            json={
+                'instance_name': globals.LOCAL_HOST,
+                'instance_ip':  globals.LOCAL_HOST_ADDRESS,
+                'instance_secret': globals.SECRET
+            }
+        )
+    except:
+        return "Error: No Connection was stablished!"
 
     return response
