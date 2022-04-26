ad0fba5cf4f93f3f22a3ee308ae128e904ee6389
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-04-25 22:35:31 -0300

    Updating Secret

--- App/mySecurity/globals.py
+++ App/mySecurity/globals.py
@@ -10,6 +10,6 @@ LOCAL_HOST = socket.gethostname()
 LOCAL_HOST_ADDRESS = socket.gethostbyname(LOCAL_HOST)
 REMOTE_HOST_ADDRESS = 'http://127.0.0.1:8000/instance'
 # ToDo: Change Secret to JWT Token
-SECRET = LOCAL_HOST + LOCAL_HOST_ADDRESS
+SECRET = LOCAL_HOST + " " + LOCAL_HOST_ADDRESS
 SECRET = base64.b64encode(SECRET.encode("utf-8"))
 SECRET = str(SECRET, "utf-8")
