3a97be8117bfa6a436a30574497a0b352464116a
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-06-27 22:08:47 -0300

    Upgrading Front-end API Requests

--- React/my-security/src/components/instance/index.tsx
+++ React/my-security/src/components/instance/index.tsx
@@ -1,21 +1,25 @@
 import React from "react"
-import { GetAllInstances } from "../../api";
+import { ShutdownInstance, AuthenticateInstance } from "../../api";
 import "./index.css";
 
 interface Props {
     name: string;
     ip: string;
+    token: string;
 }
 
-export const Instance: React.FC<Props> = ({ name, ip }) => {
+export const Instance: React.FC<Props> = ({ name, ip, token }) => {
     const handleClick = async (e: any) => {
-        console.log(await GetAllInstances());
+        const target = e.target;
+        if (target.className === "shutdown-button") { await ShutdownInstance(token); }
+        if (target.className === "auth-button") { await AuthenticateInstance(token); }
     }
     return (
         <div className="instance">
             <h3>{name}</h3>
             <p>{ip}</p>
-            <button id="shutdown" onClick={handleClick}>Shutdown</button>
+            <button className="auth-button" onClick={handleClick}>Authenticate</button>
+            <button className="shutdown-button" onClick={handleClick}>Shutdown</button>
         </div>
     )
 }
