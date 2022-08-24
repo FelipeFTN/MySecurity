502054ec9807a1efdd18ba0372415e1505117230
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-08-24 20:03:47 -0300

    📱 new interface

--- app/python/mySecurity/interface.py
+++ app/python/mySecurity/interface.py
@@ -1,8 +1,36 @@
-import kivy 
+from tkinter import *
 
-from kivy.app import App
-from kivy.uix.button import Label
+WIDTH = 300
+HEIGHT = 250
 
-class Application(App):
-    def build(self):
-        return Label(text="MySecurity!")
+
+class mySecurity(Frame):
+    def __init__(self):
+        Frame.__init__(self)
+
+        SCREEN_WIDTH = self.master.winfo_screenwidth()
+        SCREEN_HEIGHT = self.master.winfo_screenheight()
+        POSITION_X = int(SCREEN_WIDTH/2 - WIDTH/2)
+        POSITION_Y = int(SCREEN_HEIGHT/2 - HEIGHT/2)
+
+        self.pack()
+
+        self.master.title("MySecurity")
+        self.master.geometry(f"{WIDTH}x{HEIGHT}+{POSITION_X}+{POSITION_Y}")
+        self.master.resizable(False, False)
+        # self.master.iconbitmap("mySecurity.ico")
+
+        self.label0 = Label(self, text="MySecurity", width=25)
+        self.label0.pack()
+
+        self.input0 = Text(self, width=15, height=1)
+        self.input0.pack()
+
+        self.button0 = Button(self, text="Authenticate")
+        self.button0.pack()
+
+
+def main():
+    mysecurity = mySecurity()
+
+    mysecurity.mainloop()
--- app/python/mySecurity/mySecurity.py
+++ app/python/mySecurity/mySecurity.py
@@ -1,11 +1,9 @@
-# from threading import Thread
+from threading import Thread
 
 if __name__ == "__main__":
-    # Interface Kivy
-    # import interface
-    # mySecurity = interface.Application()
-    # t2 = Thread(target=mySecurity.run)
-    # t2.start()
+    import interface
+    t2 = Thread(target=interface.main)
+    t2.start()
 
     import request
     req = request.sendRequest()
