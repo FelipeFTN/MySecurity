456bcfcf82c969dc5e9f74460703a8553ad4a5e1
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-10-17 02:21:16 -0300

    ⭕ dynamic messages

--- main.c
+++ main.c
@@ -1,21 +1,37 @@
 #include <sys/socket.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 
-#include "server.h"
+#include "socket.h"
+
+char *getMessage();
 
 int main() {
     int client;
     char *option;
-    char *message = "====== MySecurity ======\n0. Shutdown Computer\n";
 
     server(&client);
 
-    sendToClient(client, message);
+    sendToClient(client, getMessage());
     
     option = receiveFromClient(client);
 
     close(client);
 
     return 0;
+}
+
+char *getMessage() {
+  char* title = "===== MySecurity =====\n";
+  char* zero = "0.Shutdown Computer\n";
+  char* one = "1.Turn off MySecurity\n";
+  char* two = "2.Exit\n";
+
+  char message[1024];
+  char *message_ptr = message;
+
+  snprintf(message, sizeof(message), "%s%s%s%s", title, zero, one, two);
+
+  return message_ptr;
 }
