22b045ce09b7d799db8a6cfac906255e0bcf296a
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-10-22 16:29:10 -0300

    ✨improving constants

---
+++ client/src/handlers/constants.js
@@ -0,0 +1,6 @@
+export const LOCAL_HOST = "192.168.1.100";
+export const SERVER_PORT = "8000";
+
+export const INSTANCE_URL = `${LOCAL_HOST}:${SERVER_PORT}/instance`;
+export const COMMAND_URL = `${LOCAL_HOST}:${SERVER_PORT}/command`;
+export const AUTH_URL = `${LOCAL_HOST}:${SERVER_PORT}/auth`;
--- client/src/handlers/instances.js
+++ client/src/handlers/instances.js
@@ -1,8 +1,9 @@
+import * as constants from "./constants";
 import axios from "axios";
 
 export const GetAllInstances = async () => {
     try {
-        const instances = await axios.get("http://localhost:8000/instance");
+        const instances = await axios.get(constants.INSTANCE_URL);
         if (instances.status !== 200) { throw instances.statusText; }
         return instances.data;
     } catch(e) { console.log(e); }
@@ -12,7 +13,7 @@ export const ShutdownInstance = async(instanceToken) => {
     try {
         if (instanceToken === "") { throw Error("Invalid Token"); }
         const config = { headers: { instance_token: instanceToken } };
-        const shutdown = await axios.post("http://localhost:8000/instance/command", {"command":"shutdown"}, config);
+        const shutdown = await axios.post(constants.COMMAND_URL, {"command":"shutdown"}, config);
         if (shutdown.status !== 200) { throw shutdown.statusText; }
         return shutdown.data;
     } catch(e) { console.log(e); }
@@ -20,7 +21,7 @@ export const ShutdownInstance = async(instanceToken) => {
 
 export const AuthenticateInstance = async (instanceToken) => {
     try {
-        const auth = await axios.get("http://localhost:8000/auth", {
+        const auth = await axios.get(constants.AUTH_URL, {
             headers: {
                 "Authorization": instanceToken
             }
